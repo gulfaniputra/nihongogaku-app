84fce0a60e1acc043458c244d32fd40bebbc75a3
Gulfani Putra <gulfaniputra@gmail.com>
2024-01-11 14:17:56 +0800

    Set up the database, model, & migration

---
+++ database/migrations/2024_01_11_061132_create_students_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->integer('id');
+            $table->unique('id');
+            $table->string('name');
+            $table->string('favorite');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
